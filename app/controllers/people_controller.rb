adf46abf053ab599be887390e2ccdc40ef263ca5
James Darling <james@abscond.org>
2017-04-05 16:59:24 +0100

    basic crud for people

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,39 @@
+class PeopleController < ApplicationController
+  def index
+    @people = Person.all
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def create
+    @person = Person.create(person_params)
+    redirect_to person_path(@person)
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    @person.update_attributes(person_params)
+    redirect_to person_path(@person)
+  end
+
+  def destroy
+    @person = Person.find(params[:id])
+    @person.destroy!
+    redirect_to people_path
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:refugee, :name, :phone_number)
+  end
+end
