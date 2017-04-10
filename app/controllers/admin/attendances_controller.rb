e1979f3b9d32dafd784de9e912a44f4b4219feaa
James Darling <james@abscond.org>
2017-04-10 17:05:29 +0100

    Add attendees from admin event page
    Creates a new attendance form, which we’ll be able to use from events
    or people.
    Allows the creation of a new person model from a new attendance.
    List more info about attendees from event show page

--- app/controllers/admin/attendances_controller.rb
+++ app/controllers/admin/attendances_controller.rb
@@ -1,13 +1,21 @@
 class Admin::AttendancesController < AdminController
-  def create
-    def create
-      @attendance = Attendance.create!(attendance_params)
-      redirect_to admin_person_path(@attendance.invitee)
+  def new
+    @attendance = Attendance.new(event_id: params[:event_id], person_id: params[:person_id])
+    if @attendance.person.blank?
+      @attendance.person = Person.new
     end
+  end
+
+  def create
+    if params[:attendance][:person_id].present?
+      params[:attendance][:person_attributes] = nil
+    end
+    @attendance = Attendance.create!(attendance_params)
+    redirect_to admin_event_path(@attendance.event)
   end
 
   private
   def attendance_params
-    params.require(:attendance).permit(:invitee_id, :event_id)
+    params.require(:attendance).permit(:invitee_id, :person_id, :event_id, person_attributes: [:name, :phone_number])
   end
 end
