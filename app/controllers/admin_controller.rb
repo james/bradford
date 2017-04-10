701cfd2fd22e462509edd66719354bf80d34428c
James Darling <james@abscond.org>
2017-04-10 17:13:38 +0100

    add basic auth to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,6 @@
 class AdminController < ApplicationController
   layout 'admin'
+  if ENV['ADMINPASSWORD']
+    http_basic_authenticate_with name: "localwelcome", password: ENV['ADMINPASSWORD']
+  end
 end
