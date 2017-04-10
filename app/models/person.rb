63a9a340092a31c0fac7025fc3a6bc1cd5707388
James Darling <james@abscond.org>
2017-04-10 17:00:50 +0100

    validate presence of name and phone number on people

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,9 @@ class Person < ApplicationRecord
   has_many :events, through: :attendances
   has_many :invitees, class_name: "Attendance", foreign_key: 'invitee_id'
 
+  validates :name, presence: true
+  validates :phone_number, presence: true
+
   def self.inheritance_column
     'something_else'
   end
